11263982683353579bcbaf825f3eab64f09245c9
Niskarsh <niskarshk@gmail.com>
2024-04-22 18:38:46 +0530

    Added init with default metadata

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,38 @@ pub mod ft_core;
 pub mod metadata;
 pub mod storage;
 
-use crate::metadata::FungibleTokenMetadata;
-
+use crate::metadata::{FungibleTokenMetadata, FT_METADATA_SPEC};
 
 #[near(contract_state)]
 pub struct Contract {
     // owner_id: AccountId,
     // total_supply: U128,
-    metadata: Option<FungibleTokenMetadata>
+    metadata: Option<FungibleTokenMetadata>,
 }
 
 #[near]
 impl Contract {
     #[init]
-    pub fn new (
-        owner_id: AccountId,
-        total_supply: U128,
-        metadata: FungibleTokenMetadata
-    ) -> Self{
-        Self { metadata: Some(metadata) }
+    pub fn new(owner_id: AccountId, total_supply: U128, metadata: FungibleTokenMetadata) -> Self {
+        Self {
+            metadata: Some(metadata),
+        }
+    }
+
+    #[init]
+    pub fn new_default_meta(owner_id: AccountId, total_supply: U128) -> Self {
+        Self::new(
+            owner_id,
+            total_supply,
+            FungibleTokenMetadata {
+                spec: FT_METADATA_SPEC.to_string(),
+                name: "My Fungible Token".to_string(),
+                symbol: "myFT".to_string(),
+                decimals: 24,
+                icon: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
     }
 }
--- src/metadata.rs
+++ src/metadata.rs
@@ -3,11 +3,14 @@ use near_sdk::json_types::Base64VecU8;
 
 #[near(serializers = [borsh, json])]
 pub struct FungibleTokenMetadata {
-    spec: String,
-    name: String,
-    symbol: String,
-    decimals: u8,
-    icon: Option<String>,
-    reference: Option<String>,
-    reference_hash: Option<Base64VecU8>,
+    pub spec: String,
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub icon: Option<String>,
+    pub reference: Option<String>,
+    pub reference_hash: Option<Base64VecU8>,
 }
+
+/// The specific version of the standard we're using
+pub const FT_METADATA_SPEC: &str = "ft-1.0.0";
