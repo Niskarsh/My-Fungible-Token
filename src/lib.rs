5c4c8c22c6d2d384acf38cc8500aa3c2af7891da
Niskarsh <niskarshk@gmail.com>
2024-04-23 23:06:24 +0530

    Adding circulating supply

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,17 @@ pub mod metadata;
 pub mod storage;
 
 // pub mod my_ft {
+    
+pub use metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC, ICON_BASE_64_ENCODED};
 
+use near_sdk::{json_types::U128, near, store::LookupMap, AccountId, PanicOnDefault};
+use near_primitives::types::Balance;
 
-pub use metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC};
-// pub(in self::test_default_metadata_for_ft_contract) use crate::metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC};
-
-use near_sdk::{json_types::U128, near, AccountId, PanicOnDefault};
 #[near(contract_state)]
 #[derive(Debug, PanicOnDefault)]
 pub struct Contract {
-    // owner_id: AccountId,
-    // total_supply: U128,
+    accounts: LookupMap<AccountId, Balance>,
+    total_supply: Balance,
     metadata: Option<FungibleTokenMetadata>,
 }
 
@@ -23,6 +23,8 @@ impl Contract {
     #[init]
     pub fn new(owner_id: AccountId, total_supply: U128, metadata: FungibleTokenMetadata) -> Self {
         Self {
+            // accounts: LookupMap::new(prefix),
+            total_supply: Balance::from(total_supply),
             metadata: Some(metadata),
         }
     }
@@ -37,7 +39,7 @@ impl Contract {
                 name: "My Fungible Token".to_string(),
                 symbol: "myFT".to_string(),
                 decimals: 24,
-                icon: None,
+                icon: Some(ICON_BASE_64_ENCODED.to_string()),
                 reference: None,
                 reference_hash: None,
             },
